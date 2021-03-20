b342896577ed07c34fac31b18ae8633db3b486b5
Lakhte <lakhteagha@gmail.com>
2021-03-19 21:02:26 -0400

    signup form

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -4,6 +4,32 @@ import { Redirect, Link } from "react-router-dom";
 class SignUpForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = this.props.form;
+
+    this.confirmed = "";
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateConfirmed = this.updateConfirmed.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.password === this.confirmed) {
+      this.props
+        .action(this.state)
+        .then(() => this.props.history.push("/videos"));
+    } else {
+    }
+  }
+
+  update(type) {
+    return (e) => this.setState({ [type]: e.target.value });
+  }
+
+  updateConfirmed(e) {
+    console.log(e.target.value);
+    this.confirmed = e.target.value;
   }
 
   renderErrors() {
@@ -21,8 +47,53 @@ class SignUpForm extends React.Component {
   }
   render() {
     return (
-      <form>
-        <h1></h1>
+      <form onSubmit={this.handleSubmit} className="signup-form">
+        <h1>
+          <span>B</span>
+          <span>o</span>
+          <span>o</span>
+          <span>g</span>
+          <span>l</span>
+          <span>e</span>
+        </h1>
+
+        <h2>Create Your Boogle Account</h2>
+        <h3>to continue to bootube</h3>
+
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={this.update("username")}
+          placeholder="Username"
+        />
+        <input
+          type="email"
+          value={this.state.email}
+          onChange={this.update("email")}
+          placeholder="Your email address"
+        />
+
+        <div className="pass">
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={this.update("password")}
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            onChange={this.updateConfirmed}
+            placeholder="Confirm"
+          />
+        </div>
+        <p>Use 6 or more characters</p>
+
+        <h5 className="errors">{this.renderErrors()}</h5>
+
+        <div className="button">
+          <Link to="/login">Sign in instead</Link>
+          <button>Next</button>
+        </div>
       </form>
     );
   }
