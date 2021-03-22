d2eddce0f7534a34cbb60bfe3530f82724ef5e58
Lakhte <lakhteagha@gmail.com>
2021-03-22 02:09:45 -0400

    bug check 1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
-class Comment < ApplicationRecord
-    validates :video_id, :user_id, :body, presence: true
+# class Comment < ApplicationRecord
+#     validates :video_id, :user_id, :body, presence: true
 
-    belongs_to :video
-    belongs_to :user
-    belongs_to :parent_comment, class_name: :Comment, optional: true
-    has_many :likes, as: :likeable
-    has_many :replies, class_name: :Comment, foreign_key: :parent_comment_id
-end
+#     belongs_to :video
+#     belongs_to :user
+#     belongs_to :parent_comment, class_name: :Comment, optional: true
+#     has_many :likes, as: :likeable
+#     has_many :replies, class_name: :Comment, foreign_key: :parent_comment_id
+# end
