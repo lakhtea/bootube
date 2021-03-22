5f77489d5425aafb9e2ca323f77a81865a7c4707
Lakhte <lakhteagha@gmail.com>
2021-03-22 02:21:50 -0400

    :/

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
-# class Like < ApplicationRecord
-#     validates :user_id,  uniqueness: { scope: [:likeable_id, :likeable_type] }
+class Like < ApplicationRecord
+    validates :user_id,  uniqueness: { scope: [:likeable_id, :likeable_type] }
 
-#     belongs_to :likeable, polymorphic: true
-#     belongs_to :user
-# end
+    belongs_to :likeable, polymorphic: true
+    belongs_to :user
+end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,12 +1,12 @@
-# class Video < ApplicationRecord
-#     validates :title, :uploader_id, :description, presence: true
-#     validates :video_url, uniqueness: true, presence: true
+class Video < ApplicationRecord
+    validates :title, :uploader_id, :description, presence: true
+    validates :video_url, uniqueness: true, presence: true
 
-#     belongs_to :user, foreign_key: :uploader_id
-#     has_many :comments, dependent: :destroy
-#     has_many :likes, as: :likeable
+    belongs_to :user, foreign_key: :uploader_id
+    has_many :comments, dependent: :destroy
+    has_many :likes, as: :likeable
 
-#     has_one_attached :vid
-#     has_one_attached :thumb
-#     has_one_attached :gif
-# end
+    has_one_attached :vid
+    has_one_attached :thumb
+    has_one_attached :gif
+end
