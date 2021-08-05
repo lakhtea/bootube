22ac637f46712549a9a0f7cccec1654ca0449b42
Lakhte <lakhteagha@gmail.com>
2021-08-05 15:15:07 -0400

    sidebar

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -31,7 +31,55 @@ class Sidebar extends React.Component {
 
   render() {
     let sideBarWidth = { width: "72px" };
-    if (this.props.sidebar) sideBarWidth = { width: "240px" };
+    if (this.props.sidebar) {
+      sideBarWidth = { width: "240px" };
+      return (
+        <div style={sideBarWidth} className="sidebar-expanded">
+          <Link
+            className={`${this.state.home ? "active" : ""}`}
+            onClick={() => this.toggleActive("home")}
+            to="/"
+          >
+            <span className="material-icons">home</span>
+            Home
+          </Link>
+          <a
+            target="_blank"
+            className={`${this.state.trending ? "active" : ""}`}
+            onClick={() => this.toggleActive("github")}
+            href="https://github.com/lakhtea"
+          >
+            <span>
+              <i className="material-icons fab fa-github"></i>
+            </span>
+            Github
+          </a>
+
+          <a
+            target="_blank"
+            className={`${this.state.subs ? "active" : ""}`}
+            onClick={() => this.toggleActive("linkedin")}
+            href="https://www.linkedin.com/in/lakhte-agha-1909b11b2"
+          >
+            <span>
+              <i className="fab fa-linkedin material-icons"></i>
+            </span>
+            LinkedIn
+          </a>
+
+          <a
+            target="_blank"
+            className={`${this.state.lib ? "active" : ""}`}
+            onClick={() => this.toggleActive("website")}
+            href="http://lakhteagha.com/"
+          >
+            <span className="material-icons">language</span>
+            Website
+          </a>
+        </div>
+      );
+    }
+
     return (
       <div style={sideBarWidth} className="sidebar">
         <Link
--- frontend/components/videos/videos_index.jsx
+++ frontend/components/videos/videos_index.jsx
@@ -4,8 +4,6 @@ import VideoItem from "./video_item";
 class VideosIndex extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { style: { paddingLeft: "90px" } };
   }
 
   componentDidMount() {
@@ -13,17 +11,12 @@ class VideosIndex extends React.Component {
   }
 
   render() {
-    console.log(this.props);
-    if (this.props.sidebar && this.state.style.paddingLeft === "90px") {
-      console.log("pee");
-      this.setState({ style: { paddingLeft: "260px" } });
-    }
-    if (!this.props.sidebar && this.state.style.paddingLeft === "260px") {
-      console.log("poop");
-      this.setState({ style: { paddingLeft: "90px" } });
-    }
+    let paddingLeft = { paddingLeft: "90px" };
+    this.props.sidebar
+      ? (paddingLeft = { paddingLeft: "260px" })
+      : (paddingLeft = { paddingLeft: "90px" });
     return (
-      <div style={this.state.style} className="video-index-container">
+      <div style={paddingLeft} className="video-index-container">
         {this.props.videos.map((video, idx) => {
           return <VideoItem key={idx} idx={idx} video={video} />;
         })}
